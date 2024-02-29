db7224817d51a068e728f6c49e45a8f1ee6cd57b
koisignal <koitokakitsubata@gmail.com>
2024-03-01 02:56:27 +0900

    astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://koi-ikeno.github.io',
+  base: '/blog',
   integrations: [mdx(), sitemap(), tailwind()]
 });
