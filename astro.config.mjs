33803edae4a85846967780db6dade96e0aa6dfaf
koisignal <koitokakitsubata@gmail.com>
2024-03-28 21:38:39 +0900

    privacy policy for apps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import nodejs from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://koi-ikeno.github.io',
   base: '/blog',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  adapter: nodejs({
+    mode: 'middleware' // または'standalone'
+  }),
+  output: 'hybrid',
 });
